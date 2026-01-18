26258a96cc4fe2dd45dfe52cbde0aa53585fc19e
arcrofts2000 <arcrofts2000@gmail.com>
2026-01-18 15:35:40 +0000

    Added comments and some more Addons for the Killers - still WIP

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// These are callable, use "." followed by the name of the variable you want, such as ".survivors"
+// Useful if you want to select a specific object in that file.
 const survivors = require('./data/survivors.json');
 const survivorItems = require('./data/survivor-items.json');
 const survivorPerks = require('./data/survivor-perks.json');
@@ -6,23 +8,26 @@ const killers = require('./data/killers.json');
 const killerPerks = require('./data/killer-perks.json');
 
 
+// Private function - generates a random number, called in all functions
 function randomNumber(length)
 {
     return Math.floor(Math.random() * length);
 }
 
 
-
+// Randomly selects a single survivor, call using ".randomSurvivor()"
 function generateSurvivorName()
 {
     return survivors[randomNumber(survivors.length)];
 }
 
+// Randomly selects a survivor perk, Call using ".getSurvivorPerk()"
 function generateSurvivorPerk()
 {
     return survivorPerks[randomNumber(survivorPerks.length)];
 }
 
+// Private Function, called in generateSurvivor()
 function generateFourSurvivorPerks()
 {
     var perkList = [];
@@ -36,6 +41,7 @@ function generateFourSurvivorPerks()
     return perkList;
 }
 
+// Randomly selects an Item with up to 2 addons, Call using ".getItem()"
 function generateItem() 
 {
     const keys = Object.keys(survivorItems);
@@ -55,6 +61,12 @@ function generateItem()
     return {"type": itemType, "addons": addons};
 }
 
+// Creates an entire loadout for Survivor, Call using ".getSurvivorLoadout()"
+/*
+    - Names the Survivor
+    - Lists 4 (different) perks
+    - Creates an item with up to 2 addons (also random)
+*/
 function generateSurvivor()
 {
     var newSurvivor = generateSurvivorName();
@@ -64,19 +76,32 @@ function generateSurvivor()
     return {"name": newSurvivor, "perks": newPerks, "item": newItem}
 }
 
+// Names a random Killer with up to 2 power addons - Call using ".randomKiller()""
 function generateKillerName()
 {
     const keys = Object.keys(killers);
     
     var killer = keys[randomNumber(keys.length)];
-    return killer;
+
+    var numOfAddons = randomNumber(3);
+    var addonList = [];
+    while (addonList.length < numOfAddons)
+    {
+        var newAddon = killers[killer].addon[randomNumber(killers[killer].addon.length)];
+        if (!addonList.includes(newAddon))
+            addonList.push(newAddon);
+    }
+
+    return {"name": killer, "addons": addonList};
 }
 
+// Names a random killer perk - Call using ".getKillerPerk()"
 function generateKillerPerk()
 {
     return killerPerks[randomNumber(killerPerks.length)];
 }
 
+// Private Function - Used in generateKiller() to list 4 different perks
 function generateFourKillerPerks()
 {
     var perkList = [];
@@ -89,6 +114,12 @@ function generateFourKillerPerks()
     return perkList;
 }
 
+// Creates a whole killer loadout - Call using ".getKillerLoadout()"
+/*
+    - Names a random Killer
+    - Lists 4 different killer perks
+    - creates up to 2 addons for the Killer Power (based on the Killer named)
+*/
 function generateKiller()
 {
     var killer = generateKillerName();
@@ -106,17 +137,18 @@ function generateKiller()
     return {"name": killer, "perkList": killerPerkList, "addons": addonList}
 }
 
-console.log(generateSurvivor());
 
-exports.allSurvivors = survivors;
+
+exports.survivors = survivors;
+exports.items = survivorItems;
+exports.survivorPerks = survivorPerks;
 exports.randomSurvivor = generateSurvivorName;
-exports.allItems = survivorItems;
-exports.allSurvivorPerks = survivorPerks;
+exports.getItem = generateItem;
 exports.getSurvivorPerk = generateSurvivorPerk;
 exports.getSurvivorLoadout = generateSurvivor;
 
-exports.allKillers = killers;
+exports.killers = killers;
+exports.killerPerks = killerPerks;
 exports.randomKiller = generateKillerName;
-exports.allKillerPerks = killerPerks;
 exports.getKillerPerk = generateKillerPerk;
-exports.getKillerLoadout = generateFourKillerPerks;
+exports.getKillerLoadout = generateKiller;
